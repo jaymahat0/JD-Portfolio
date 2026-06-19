bbbe34ab5244b75a5fe78427034f6fefaeb629a5
jaymahat0 <jaymahato.333@gmail.com>
2026-06-19 11:16:51 +0530

    Completed the version v1 with Flyway Migration

--- src/main/java/com/jd/portfolio/controller/HomeController.java
+++ src/main/java/com/jd/portfolio/controller/HomeController.java
@@ -1,4 +1,14 @@
 package com.jd.portfolio.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
 public class HomeController {
+    @GetMapping
+    public String home() {
+        return "Portfolio Backend Running Successfully";
+    }
 }
